7be14cd2f2deb5efb109245ead3f3cd48c1f19cd
Shanice Chebet Kenduiwa <kenduiwashanice63@gmail.com>
2024-03-27 21:14:39 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(driversArray, query) {
+    return driversArray.filter(driver => driver.toLowerCase() === query.toLowerCase());
+}
+
+// Test
+//console.log(findMatching(['John', 'Jane', 'Jack', 'Jason'], 'jane')); // Output: ['Jane']//
+function fuzzyMatch(driversArray, query) {
+    return driversArray.filter(driver =>driver.startsWith(query) );
+}
+function matchName(driversArray, query) {
+    return driversArray.filter(driver => driver.name === query);
+}
+//Test
+console.log(fuzzyMatch(['John', 'Jane', 'Jack', 'Jason'], 'Ja')); // Output: ['Jane', 'Jack', 'Jason']
+console.log(matchName(drivers, 'John')); //Output: [{ name: 'John', hometown: 'New York' }]
